2653d4df6e079231d35e888a568e535074d39469
Dominique Campitelli <dominiquecamp@outlook.com>
2024-09-28 00:43:54 -0300

    Structure with vite, typescript and origin vite plugin federation

---
+++ host/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Login from "login/App";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ login/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "login",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/main",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
